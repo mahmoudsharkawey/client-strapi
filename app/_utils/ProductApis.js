19ff3add781cb01aee5a01ffecd2a8dd22aafad3
mahmoud-gamall <144719496+mahmoudsharkawey@users.noreply.github.com>
2024-02-21 18:59:47 +0200

    wdwd

--- app/_utils/ProductApis.js
+++ app/_utils/ProductApis.js
@@ -1,6 +1,6 @@
 const { default: axiosClient } = require("./axiosClient");
 
-const getLatestProducts = () => axiosClient.get("/products");
+const getLatestProducts = () => axiosClient.get("/products?populate=*");
 const getProductById = (id) => axiosClient.get(`/products/${id}?populate=*`);
 const getProductsByCategory = (category)=>axiosClient.get(`/products?filters[category][$eq]=${category}&populate=*`)
 
